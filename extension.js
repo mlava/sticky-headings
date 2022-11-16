4b43fbcd8df7acfcc6b0db6929456637aebe872e
Mark Lavercombe <mlava@users.noreply.github.com>
2022-11-17 08:26:48 +1100

    Add files via upload

--- extension.js
+++ extension.js
@@ -18,6 +18,9 @@ export default {
         window.addEventListener('keydown', myEventHandler, false);
     },
     onunload: () => {
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Toggle Sticky Headings'
+        });
         var head = document.getElementsByTagName("head")[0];
         if (document.getElementById("sticky-css")) {
             var cssStyles = document.getElementById("sticky-css");
@@ -39,7 +42,6 @@ function RGBAToHexA(rgba, forceRemoveAlpha) { // courtesy of Lars Flieger at htt
 }
 
 function stickyHeadingsToggle() {
-    console.info(stickyHeadingsState);
     if (stickyHeadingsState == false) {
         stickyHeadingsOn();
     } else {
@@ -48,50 +50,66 @@ function stickyHeadingsToggle() {
 }
 
 function stickyHeadingsOn() {
+    var comph1, comph2, comph3, appBG, h1BG, h2BG, h3BG, h1CSS, h2CSS, h3CSS;
+    var h2Margin = 0;
+    var h3Margin = 0;
+    var cssString = "";
+
     const app = document.querySelector(".roam-body .roam-app");
     const compApp = window.getComputedStyle(app);
-    const h1 = document.querySelector(".rm-heading-level-1 > .rm-block-main.rm-block__self:first-child");
-    const comph1 = window.getComputedStyle(h1);
-    const h2 = document.querySelector(".rm-heading-level-2 > .rm-block-main.rm-block__self:first-child");
-    const comph2 = window.getComputedStyle(h2);
-    const h3 = document.querySelector(".rm-heading-level-3 > .rm-block-main.rm-block__self:first-child");
-    const comph3 = window.getComputedStyle(h3);
-    let h2Margin = parseInt(comph1.height) - 1;
-    let h3Margin = parseInt(comph1.height) - 1 + parseInt(comph2.height);
-
-    var appBG, h1BG, h2BG, h3BG;
-
     if (compApp["backgroundColor"] == "rgba(0, 0, 0, 0)") {
         appBG = "white";
     } else {
         appBG = RGBAToHexA(compApp["backgroundColor"], true);
     }
-    if (comph1["backgroundColor"] == "rgba(0, 0, 0, 0)" || comph1["backgroundColor"] == "rgb(255, 255, 255)") {
-        h1BG = appBG;
-    } else {
-        h1BG = RGBAToHexA(comph1["backgroundColor"], true);
-    }
-    if (comph2["backgroundColor"] == "rgba(0, 0, 0, 0)") {
-        h2BG = appBG;
-    } else {
-        h2BG = RGBAToHexA(comph2["backgroundColor"], true);
-    }
-    if (comph3["backgroundColor"] == "rgba(0, 0, 0, 0)") {
-        h3BG = appBG;
-    } else {
-        h3BG = RGBAToHexA(comph3["backgroundColor"], true);
-    }
 
+    const h1 = document.querySelector(".rm-heading-level-1 > .rm-block-main.rm-block__self:first-child");
+    const h2 = document.querySelector(".rm-heading-level-2 > .rm-block-main.rm-block__self:first-child");
+    const h3 = document.querySelector(".rm-heading-level-3 > .rm-block-main.rm-block__self:first-child");
+    if (h1 != null) {
+        comph1 = window.getComputedStyle(h1);
+        if (comph1["backgroundColor"] == "rgba(0, 0, 0, 0)" || comph1["backgroundColor"] == "rgb(255, 255, 255)") {
+            h1BG = appBG;
+        } else {
+            h1BG = RGBAToHexA(comph1["backgroundColor"], true);
+        }
+        h2Margin = parseInt(comph1.height) - 1;
+        h3Margin = parseInt(comph1.height) - 1;
+        h1CSS = ".rm-heading-level-1 > .rm-block-main.rm-block__self:first-child {background-color: " + h1BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 18 !important; top: -1px !important;}";
+        cssString += h1CSS;
+    } else {
+        h2Margin = h2Margin - 1;
+        h3Margin = h3Margin - 1;
+    }
+    if (h2 != null) {
+        comph2 = window.getComputedStyle(h2);
+        if (comph2["backgroundColor"] == "rgba(0, 0, 0, 0)") {
+            h2BG = appBG;
+        } else {
+            h2BG = RGBAToHexA(comph2["backgroundColor"], true);
+        }
+        h2CSS = ".rm-heading-level-2 > .rm-block-main.rm-block__self:first-child {background-color: " + h2BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 17 !important; top: " + h2Margin + "px !important;}";
+        cssString += h2CSS;
+        h3Margin = h3Margin + parseInt(comph2.height);
+    }
+    if (h3 != null) {
+        comph3 = window.getComputedStyle(h3);
+        if (comph3["backgroundColor"] == "rgba(0, 0, 0, 0)") {
+            h3BG = appBG;
+        } else {
+            h3BG = RGBAToHexA(comph3["backgroundColor"], true);
+        }
+        h3CSS = ".rm-heading-level-3 > .rm-block-main.rm-block__self:first-child {background-color: " + h3BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 16 !important; top: " + h3Margin + "px !important;}";
+        cssString += h3CSS;
+    }
     // CSS adapted and modified from a post on Slack shared by Fabrice Gallet https://roamresearch.slack.com/archives/C016N2B66JU/p1667846823724739?thread_ts=1667816335.849789&cid=C016N2B66JU
-    let h1CSS = ".rm-heading-level-1 > .rm-block-main.rm-block__self:first-child {background-color: " + h1BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 18 !important; top: -1px !important;}";
-    let h2CSS = ".rm-heading-level-2 > .rm-block-main.rm-block__self:first-child {background-color: " + h2BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 17 !important; top: " + h2Margin + "px !important;}";
-    let h3CSS = ".rm-heading-level-3 > .rm-block-main.rm-block__self:first-child {background-color: " + h3BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 16 !important; top: " + h3Margin + "px !important;}";
-    let roamFixCSS = ".rm-article-wrapper {margin-top: -2px !important;}";
 
+    let roamFixCSS = ".rm-article-wrapper {margin-top: -2px !important;}";
+    cssString += roamFixCSS;
     var head = document.getElementsByTagName("head")[0];
     var style = document.createElement("style");
     style.id = "sticky-css";
-    style.textContent = h1CSS + h2CSS + h3CSS + roamFixCSS;
+    style.textContent = cssString;
     head.appendChild(style);
     stickyHeadingsState = true;
 }
