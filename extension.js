d0fce9837cb718bfbe5606be13c05b6e81d80571
Mark Lavercombe <mlava@users.noreply.github.com>
2023-01-27 14:29:53 +1100

    Add files via upload

--- extension.js
+++ extension.js
@@ -50,9 +50,12 @@ function stickyHeadingsToggle() {
 }
 
 function stickyHeadingsOn() {
-    var comph1, comph2, comph3, appBG, h1BG, h2BG, h3BG, h1CSS, h2CSS, h3CSS;
+    var comph1, comph2, comph3, appBG, h1BG, h2BG, h3BG, h1CSS, h2CSS, h3CSS, h4BG, h5BG, h6BG;
     var h2Margin = 0;
     var h3Margin = 0;
+    var h4Margin = 0;
+    var h5Margin = 0;
+    var h6Margin = 0;
     var cssString = "";
 
     const body = document.body;
@@ -82,11 +85,17 @@ function stickyHeadingsOn() {
         }
         h2Margin = parseInt(comph1.height) - 1;
         h3Margin = parseInt(comph1.height) - 1;
-        h1CSS = ".rm-heading-level-1 > .rm-block-main.rm-block__self:first-child {background-color: " + h1BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 18 !important; top: -1px !important;}";
+        h4Margin = parseInt(comph1.height) - 1;
+        h5Margin = parseInt(comph1.height) - 1;
+        h6Margin = parseInt(comph1.height) - 1;
+        h1CSS = ".rm-heading-level-1 > .rm-block-main.rm-block__self:first-child {background-color: " + h1BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 18 !important; top: -1px !important;} ";
         cssString += h1CSS;
     } else {
         h2Margin = h2Margin - 1;
         h3Margin = h3Margin - 1;
+        h4Margin = h4Margin - 1;
+        h5Margin = h5Margin - 1;
+        h6Margin = h6Margin - 1;
     }
     if (h2 != null) {
         comph2 = window.getComputedStyle(h2);
@@ -97,9 +106,9 @@ function stickyHeadingsOn() {
         } else {
             h2BG = RGBAToHexA(comph2["backgroundColor"], true);
         }
-        h2CSS = ".rm-heading-level-2 > .rm-block-main.rm-block__self:first-child {background-color: " + h2BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 17 !important; top: " + h2Margin + "px !important;}";
+        h2CSS = ".rm-heading-level-2 > .rm-block-main.rm-block__self:first-child {background-color: " + h2BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 17 !important; top: " + h2Margin + "px !important;} ";
         cssString += h2CSS;
-        h3Margin = h3Margin + parseInt(comph2.height);
+        h3Margin = h2Margin + parseInt(comph2.height);
     }
     if (h3 != null) {
         comph3 = window.getComputedStyle(h3);
@@ -110,8 +119,51 @@ function stickyHeadingsOn() {
         } else {
             h3BG = RGBAToHexA(comph3["backgroundColor"], true);
         }
-        h3CSS = ".rm-heading-level-3 > .rm-block-main.rm-block__self:first-child {background-color: red /*" + h3BG + "*/ !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 16 !important; top: " + h3Margin + "px !important;}";
+        h3CSS = ".rm-heading-level-3 > .rm-block-main.rm-block__self:first-child {background-color: " + h3BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 16 !important; top: " + h3Margin + "px !important;} ";
         cssString += h3CSS;
+        h4Margin = h3Margin + parseInt(comph3.height);
+    }
+    if (document.querySelector("[data-tag^='h4'] + .rm-highlight")) {
+        const h4 = document.querySelector("[data-page-links^='[\"h4\"]'] > .rm-block-main.rm-block__self:first-child");
+        var comph4 = window.getComputedStyle(h4);
+        console.info(comph4["height"])
+        if (body.classList[0] == "bp3-dark") { // this is Roam "Native" Dark dark mode
+            h4BG = "#30404d";
+        } else if (comph4["backgroundColor"] == "rgba(0, 0, 0, 0)" || comph4["backgroundColor"] == "rgb(255, 255, 255)") {
+            h4BG = appBG;
+        } else {
+            h4BG = RGBAToHexA(comph4["backgroundColor"], true);
+        }
+        var h4CSS = "[data-page-links^='[\"h4\"]'] > .rm-block-main.rm-block__self:first-child {background-color: " + h4BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 15 !important; top: " + h4Margin + "px !important;} ";
+        cssString += h4CSS;
+        h5Margin = h4Margin + parseInt(comph4.height);
+    }
+    if (document.querySelector("[data-tag^='h5'] + .rm-highlight")) {
+        const h5 = document.querySelector("[data-page-links^='[\"h5\"]'] > .rm-block-main.rm-block__self:first-child");
+        var comph5 = window.getComputedStyle(h5);
+        if (body.classList[0] == "bp3-dark") { // this is Roam "Native" Dark dark mode
+            h5BG = "#30404d";
+        } else if (comph5["backgroundColor"] == "rgba(0, 0, 0, 0)" || comph5["backgroundColor"] == "rgb(255, 255, 255)") {
+            h5BG = appBG;
+        } else {
+            h5BG = RGBAToHexA(comph5["backgroundColor"], true);
+        }
+        var h5CSS = "[data-page-links^='[\"h5\"]'] > .rm-block-main.rm-block__self:first-child {background-color: " + h5BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 14 !important; top: " + h5Margin + "px !important;} ";
+        cssString += h5CSS;
+        h6Margin = h5Margin + parseInt(comph5.height);
+    }
+    if (document.querySelector("[data-tag^='h6'] + .rm-highlight")) {
+        const h6 = document.querySelector("[data-page-links^='[\"h6\"]'] > .rm-block-main.rm-block__self:first-child");
+        var comph6 = window.getComputedStyle(h6);
+        if (body.classList[0] == "bp3-dark") { // this is Roam "Native" Dark dark mode
+            h6BG = "#30404d";
+        } else if (comph6["backgroundColor"] == "rgba(0, 0, 0, 0)" || comph6["backgroundColor"] == "rgb(255, 255, 255)") {
+            h6BG = appBG;
+        } else {
+            h6BG = RGBAToHexA(comph6["backgroundColor"], true);
+        }
+        var h6CSS = "[data-page-links^='[\"h6\"]'] > .rm-block-main.rm-block__self:first-child {background-color: " + h6BG + " !important; opacity: 1.0 !important; will-change: transform !important; position: sticky !important; z-index: 13 !important; top: " + h6Margin + "px !important;} ";
+        cssString += h6CSS;
     }
     // CSS adapted and modified from a post on Slack shared by Fabrice Gallet https://roamresearch.slack.com/archives/C016N2B66JU/p1667846823724739?thread_ts=1667816335.849789&cid=C016N2B66JU
 
